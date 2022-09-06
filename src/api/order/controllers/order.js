543e559e12bc42d451ea20498454b2c97c2cef6f
Rehman ali <rehmanaliawans@gmail.com>
2022-09-06 18:52:38 +0500

    fixed create method issue in order create controller

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -3,12 +3,12 @@
 /**
  * order controller
  */
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { createCoreController } = require("@strapi/strapi").factories;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
-    const { amount, shippingAddress, city, state, pin, token } =
+    const { amount, shippingAddress, city, state, pin, token, items } =
       ctx.request.body;
     await stripe.charges.create({
       amount: amount * 100,
@@ -17,13 +17,15 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       description: `Order by user ${ctx.state.user.email}`,
     });
     const order = await strapi.db.query("api::order.order").create({
-      shippingAddress,
-      amount,
-      items,
-      pin,
-      city,
-      state,
-      user: ctx.state.user.email,
+      data: {
+        shippingAddress,
+        amount,
+        items,
+        pin,
+        city,
+        state,
+        user: ctx.state.user.email,
+      },
     });
     return order;
   },
